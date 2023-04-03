884a0bf66913e889657cd7bf1f1cc4fbdf072a03
dragoscrisan7 <dragoscrisan.ubb@gmail.com>
2023-04-03 22:18:57 +0300

    fixed table

--- Frontend/src/components/StudentsShowAll.tsx
+++ Frontend/src/components/StudentsShowAll.tsx
@@ -18,24 +18,28 @@ export const StudentsShowAll = () => {
         <div className="App">
             <h1>Students List</h1>
             <table>
-                <tr>
-                    <th>#</th>
-                    <th>Firstname</th>
-                    <th>Name</th>
-                    <th>CNP</th>
-                    <th>Favorite color</th>
-                    <th>GroupId</th>
-                </tr>
-                {students.map((student: Student, index) => (
+                <thead>
                     <tr>
-                        <td>{index}</td>
-                        <td>{student.firstname}</td>
-                        <td>{student.name}</td>
-                        <td>{student.CNP}</td>
-                        <td>{student.favorite_colour}</td>
-                        <td>{student.GroupId}</td>
+                        <th>#</th>
+                        <th>Firstname</th>
+                        <th>Name</th>
+                        <th>CNP</th>
+                        <th>Favorite color</th>
+                        <th>GroupId</th>
                     </tr>
-                ))}
+                </thead>
+                <tbody>
+                    {students.map((student: Student, index) => (
+                        <tr key={student.id}>
+                            <td>{index}</td>
+                            <td>{student.firstname}</td>
+                            <td>{student.name}</td>
+                            <td>{student.CNP}</td>
+                            <td>{student.favorite_colour}</td>
+                            <td>{student.GroupId}</td>
+                        </tr>
+                    ))}
+                </tbody>
             </table>
         </div>
     );
