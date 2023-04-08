a9249bbc1ebfeb4a53cd6c805d9f90cf49ca5523
dragoscrisan7 <dragoscrisan.ubb@gmail.com>
2023-04-08 23:00:41 +0300

    Frontend startup

--- Frontend/src/constants.ts
+++ Frontend/src/constants.ts
@@ -1,5 +1,5 @@
-const PROD_BACKEND_API_URL = "https://sdi-2023-example.strangled.net/api";
-const DEV_BACKEND_API_URL = "http://127.0.0.1:8000/api";
+const PROD_BACKEND_API_URL = "https://sdi-2023-example.strangled.net";
+const DEV_BACKEND_API_URL = "http://13.50.240.137:8000";
 
 export const BACKEND_API_URL =
 	process.env.NODE_ENV === "development" ? DEV_BACKEND_API_URL : PROD_BACKEND_API_URL;
